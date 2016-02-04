fcb6e6513f30464db75967074ea23001073b2c33
netznarkose <mmaintz@posteo.de>
2016-02-04 19:02:05 +0100

    robin session

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ActiveRecord::Base
   # has_many :entries, dependent: :destroy
   # has_many :entries, dependent: { user_id: 1 }
+  after_destroy :assign_remaining_entries_to_superadmin
   has_many :entries
   has_many :comments
   # Include default devise modules. Others available are:
@@ -12,11 +13,13 @@ class User < ActiveRecord::Base
   validates :name, presence: true
   validates :email, presence: true
 
-  scope :allowed_for_entries, ->{ where( role: ['admin', 'editor']) }
+  scope :allowed_for_entries, ->{ where( role: ['superadmin', 'admin', 'editor']) }
+
+
 
 
   def admin?
-    role == "admin"
+    role == "admin" || role == 'superadmin'
   end
 
   def editor?
@@ -27,12 +30,9 @@ class User < ActiveRecord::Base
     self.entries.where("japanische_umschrift LIKE ? OR kanji LIKE ? OR namenskuerzel = ? OR kennzahl = ? OR romaji_order LIKE ?", "%#{query}%", "%#{query}%", "#{query}", "#{query}", "%#{query}%")
   end
 
-  def assign_remaining_entries_to_user_one 
-    Entry.where(user_id: nil).map do |entry|
-      entry.user_id = 1 
-      entry.save
-    end
+  def assign_remaining_entries_to_superadmin 
+    super_admin = User.find_by_role('superadmin')
+    super_admin.entries << self.entries
+    super_admin.save
   end
-
-
 end
