eb666cc2cc6086302b71faec37b3d43aabb1b1a1
netznarkose <mmaintz@posteo.de>
2016-03-05 17:07:22 +0100

    routes.rb two old routes readded

--- config/routes.rb
+++ config/routes.rb
@@ -19,6 +19,8 @@ DGLB::Application.routes.draw do
   
   resources :users
   resources :users_admin, controller: :users
+  match "users/:id/update_role" => "users#update_role", :as => "update_user_role", :via => :put       
+  match "users/:id/entries" => "users#entries", :as => "users_entries", :via => :get
   post "versions/:id/revert" => "versions#revert", :as => "revert_version"
 
   resources :entries
