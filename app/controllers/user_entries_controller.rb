484870f8526cb8bd6de3835905aa2f173a8e55f1
netznarkose <mmaintz@posteo.de>
2016-03-10 15:29:49 +0100

    user specific search and sort now implemented

--- app/controllers/user_entries_controller.rb
+++ app/controllers/user_entries_controller.rb
@@ -1,10 +1,19 @@
 class UserEntriesController < ApplicationController
   def index
     @user = User.find(params[:user_id])
-    unless params[:search].empty?
+    search_me = params[:search]
+    unless search_me == nil || search_me == ""
       @user_entries = @user.entries.search(params[:search])
     else
-      @user_entries = @user.entries.all
+      @user_entries = @user.entries.order(sort_column + " " + sort_direction).page(params[:page])
     end
+  end
+  private
+  def sort_column
+    Entry.column_names.include?(params[:sort]) ? params[:sort] : "kennzahl"
+  end
+  
+  def sort_direction
+    %w[asc desc].include?(params[:direction]) ? params[:direction] : "desc"
   end
 end
