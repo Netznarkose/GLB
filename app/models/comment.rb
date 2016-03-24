06dc3c15b2574d0a5ab9f61c28da7d0e12a6f42a
netznarkose <mmaintz@posteo.de>
2016-03-24 18:47:30 +0100

    updated models/comment.rb and comment_spec.rb

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,4 +2,7 @@ class Comment < ActiveRecord::Base
   belongs_to :user
   belongs_to :entry
   # attr_accessible :entry_id, :user_id, :comment
+  validates :comment, presence: true
+  validates :user_id, presence: true
+  validates :entry_id, presence: true
 end
