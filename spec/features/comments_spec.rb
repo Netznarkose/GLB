b00ce65388ae212fd022ef8abb35e465e46b0183
netznarkose <mmaintz@posteo.de>
2016-03-31 18:32:26 +0200

    added a edit-test to features/comments_spec

--- spec/features/comments_spec.rb
+++ spec/features/comments_spec.rb
@@ -1,32 +1,43 @@
 require 'spec_helper'
 
-describe "comments management api" do
-  describe "admin write a comment" do
+describe 'comments management api' do
+  describe 'admin write a comment' do
     let(:admin) { FactoryGirl.create(:admin) }
     let(:entry) { FactoryGirl.create(:entry) }
     let(:comment) { FactoryGirl.create(:comment) }
     before(:each) do
       visit new_user_session_path
-      fill_in "user_email", with: admin.email
-      fill_in "user_password", with: admin.password
+      fill_in 'user_email', with: admin.email
+      fill_in 'user_password', with: admin.password
       click_button('Anmelden')
     end
-    it "should display a certain Entry" do
+    it 'should display a certain Entry' do
       visit entry_path(entry)
-      page.should have_content("Scan SBDJ")
+      expect(page).to have_content('Scan SBDJ')
     end
-    it "should be able to write and save a comment" do
+    it 'should be able to write and save a comment' do
       visit entry_path(entry)
-      fill_in "comment_comment", with: comment
-      click_button("Speichern")
-      page.should have_content(comment)
+      fill_in 'comment_comment', with: comment
+      click_button('Speichern')
+      expect(page).to have_content(comment)
     end
-    it "should see an error message when try's 
-    to save a comment without text" do
+    it 'should see an error message when tries
+    to save a comment without text' do
       visit entry_path(entry)
-      fill_in "comment_comment", with: "" 
-      click_button("Speichern")
-      page.should have_content("The form contains 1 error.")
+      fill_in 'comment_comment', with: ''
+      click_button('Speichern')
+      expect(page).to have_content('The form contains 1 error.')
+    end
+    it 'should be able to edit  a comment' do
+      # writes a comment to be able to edit it
+      visit entry_path(entry)
+      fill_in 'comment_comment', with: comment
+      click_button('Speichern')
+      # edits the comment
+      find('.right-action', match: :first).click # Clicks 'Bearbeiten' which is ambiguous
+      fill_in 'comment_comment', with: 'some changes in the comment'
+      click_button('Speichern')
+      expect(page).to have_content('some changes in the comment')
     end
   end
 end
