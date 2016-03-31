2367085f7c56194f94c1ec4c38da6834d8340e1e
netznarkose <mmaintz@posteo.de>
2016-03-31 10:43:11 +0200

    refactoring of comments_controller and comments_controller_spec

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,47 +1,15 @@
 #encoding: utf-8
 class CommentsController < ApplicationController
+  before_action :find_comment, only: [:edit, :update, :destroy]
 
-  # GET /comments
-  # GET /comments.json
-  def index
-    @comments = Comment.all
-
-    respond_to do |format|
-      format.html # index.html.erb
-      format.json { render json: @comments }
-    end
-  end
-
-  # GET /comments/1
-  # GET /comments/1.json
-  def show
-    @comment = Comment.find(params[:id])
-    redirect_to entry_path(@comment.entry) + "#comments"
-  end
-
-  # GET /comments/new
-  # GET /comments/new.json
-  def new
-    @comment = Comment.new
-
-    respond_to do |format|
-      format.html # new.html.erb
-      format.json { render json: @comment }
-    end
-  end
-
-  # GET /comments/1/edit
   def edit
-    @comment = Comment.find(params[:id])
     @entry = @comment.entry
-    if @comment.user != current_user && current_user.role != "admin"
-      flash[:notice] = 'Access denied!'
-      redirect_to entry_path(@comment.entry) + "#comments"
-    end
+    # if @comment.user != current_user && current_user.role != "admin"
+    #   flash[:notice] = 'Access denied!'
+      redirect_to entry_path(@comment.entry)
+    # end
   end
 
-  # POST /comments
-  # POST /comments.json
   def create
     @comment = current_user.comments.build(comment_params)
     @entry = @comment.entry # is necessary for the rendering of 'entries/show'
@@ -56,31 +24,24 @@ class CommentsController < ApplicationController
     end
   end
 
-  # PUT /comments/1
-  # PUT /comments/1.json
   def update
-    @comment = Comment.find(params[:id])
-
     respond_to do |format|
       if @comment.update_attributes(comment_params)
         format.html { redirect_to entry_path(@comment.entry) + "#comments", notice: 'Kommentar erfolgreich bearbeitet.' }
         format.json { head :no_content }
       else
+        raise
         format.html { render action: "edit" }
         format.json { render json: @comment.errors, status: :unprocessable_entity }
       end
     end
   end
 
-  # DELETE /comments/1
-  # DELETE /comments/1.json
   def destroy
-    @comment = Comment.find(params[:id])
     @entry = @comment.entry
     @comment.destroy
-
     respond_to do |format|
-      format.html { redirect_to @entry, notice: 'Kommentar erfolgreich gelöscht.'}
+      format.html { redirect_to entry_path(@comment.entry), notice: 'Kommentar erfolgreich gelöscht.'}
       format.json { head :no_content }
     end
   end
@@ -88,5 +49,9 @@ class CommentsController < ApplicationController
   private
     def comment_params
       params.require(:comment).permit(:comment,:user_id,:entry_id)
+    end
+
+    def find_comment
+      @comment = Comment.find(params[:id])
     end
 end
--- spec/controllers/comments_controller_spec.rb
+++ spec/controllers/comments_controller_spec.rb
@@ -1,35 +1,53 @@
 require 'spec_helper'
 
-
-describe CommentsController, :type => :controller do
-
+describe CommentsController, type: :controller do
   let(:comment) { FactoryGirl.create(:comment) }
   let(:admin) { FactoryGirl.create(:admin) }
-  let(:editor) { FactoryGirl.create(:editor) }
-  let(:user) { FactoryGirl.create(:user) }
 
   before :each do
     admin
   end
-  describe "POST create" do
-    context "Admin is able to create a comment" do
+
+  describe 'get edit' do
+    context 'Admin is able to edit a comment' do
+      it 'Edits a comment' do
+        sign_in admin 
+        get :edit, id: comment.id
+        expect(response).to redirect_to(entry_path(comment.entry))
+      end
+    end
+  end
+  describe 'get update' do
+    context 'Admin is able to update a Comment' do
+      it 'Admin is able to update a Comment' do
+        sign_in admin 
+        put :update, id: comment.id, comment: { comment: 'hey some changes in the content' }
+        comment.reload
+        expect(comment.comment).to eq('hey some changes in the content')
+      end
+    end
+  end
+
+
+  describe 'POST create' do
+    context 'Admin is able to create a comment' do
       before :each do
         sign_in admin
       end
 
-      it "creates a valid comment when entering 
-      valid data" do
-        attributes = FactoryGirl.attributes_for(:comment) 
+      it 'creates a valid comment when entering
+      valid data' do
+        attributes = FactoryGirl.attributes_for(:comment)
         expect {
           post :create, comment: attributes
         }.to change(Comment, :count).by(1)
-        assigns(:entry).tap do |entry|
+        assigns(:comment).tap do |comment|
           expect(comment).to be_valid
         end
       end
-      it "does not creates a comment when entering 
-      invalid data" do
-        attributes = FactoryGirl.attributes_for(:comment, comment: "") 
+      it 'does not creates a comment when entering
+      invalid data' do
+        attributes = FactoryGirl.attributes_for(:comment, comment: '')
         expect {
           post :create, comment: attributes
         }.to change(Comment, :count).by(0)
@@ -37,6 +55,20 @@ describe CommentsController, :type => :controller do
       end
     end
   end
+  describe 'DELETE destroy' do
+    before do
+      admin
+      comment
+    end
+    context 'Admin is able to delete a comment' do
+      it 'I deletes a comment' do
+        sign_in admin
+        expect{
+          delete :destroy, id: comment.id
+        }.to change(Comment, :count).by(-1)
+        expect(response).to redirect_to(entry_path(comment.entry))
+        expect(flash[:notice]).not_to be_empty
+      end
+    end
+  end
 end
-
-
