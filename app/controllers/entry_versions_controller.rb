011d77889c9d956c57268c2b903df67c10b6f554
netznarkose <mmaintz@posteo.de>
2016-04-18 17:55:33 +0200

    big success

--- app/controllers/entry_versions_controller.rb
+++ app/controllers/entry_versions_controller.rb
@@ -1,28 +1,29 @@
 class EntryVersionsController < ApplicationController
   def index
-    @entry_versions_container = []
+    @entry_versions_all = []
     find_current_entry
-    @entry_version = @entry.versions.size
-    entry_versions_count = @entry.versions.size
-    if entry_versions_count > 1
-      1.upto(entry_versions_count) do |count_me|
-        @entry_versions_container << @entry.versions[- count_me]
+    if @entry.versions.size > 1
+      1.upto(@entry.versions.size) do |count_me|
+        @entry_versions_all << @entry.versions[- count_me]
       end
     end
   end
   def show
-    find_current_entry_version
-    # @entry = Entry.find(params[:entry_id])
-    # @entry_version  = @entry.entry_versions.find(params[:id])
+    @entry = Entry.find(params[:entry_id])
+    version_number = params[:id].to_i
+    @entry_version  = @entry.versions[- version_number]
+    binding.pry
   end
 
   def find_current_entry
-    binding.pry
     @entry = Entry.find(params[:entry_id])
+  end
+  def find_current_entry2
+    @entry = PaperTrail::Version.find(params[:item_id])
   end
   def find_current_entry_version
     binding.pry
-    @entry = Entry.find(params[:entry_id][:id])
+    @entry = Entry.find(params[:entry_id])
     @entry_version  = @entry.entry.versions.find_by(params[:id])
   end
 end
