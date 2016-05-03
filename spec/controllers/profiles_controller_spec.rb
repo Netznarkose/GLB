dbfa2f887bb36920dfc263e312721b61ecec2bff
netznarkose <mmaintz@posteo.de>
2016-05-03 17:49:18 +0200

    refactored profiles_controller_spec

--- spec/controllers/profiles_controller_spec.rb
+++ spec/controllers/profiles_controller_spec.rb
@@ -6,42 +6,57 @@ describe ProfilesController, type: :controller do
   let(:user) { FactoryGirl.create(:user) }
 
   describe 'get edit' do
-    context 'as currently logged in editor' do
-      it 'I can render my #edit-view' do
+    context 'as logged-in user' do
+      it 'I can render the edit-template' do
         sign_in editor
         get :edit, id: editor.id
         expect(response).to render_template(:edit)
       end
     end
+    context 'as non-logged-in user' do
+      it 'I can not render the edit-template' do
+        get :edit, id: editor.id
+        expect(response).not_to render_template(:edit)
+      end
+    end
   end
 
   describe 'get update' do
-    context 'as currently logged in editor' do
-      it 'I can update my name and email and get a confirmation-message' do
-        sign_in editor
-        put :update, id: editor.id, user: { name: 'different_editor name',
-                                            email: 'different_editor@user.com' }
-        editor.reload
-        expect(editor.name).to eq('different_editor name')
-        expect(editor.email).to eq('different_editor@user.com')
-        expect(flash[:notice]).to eq('profile updated')
+    context 'as logged-in user' do
+      context 'with valid attributes' do
+        before do
+          sign_in editor
+          put :update, id: editor.id, user: { name: 'different_editor name',
+                                              email: 'different_editor@user.com' }
+        end
+        it 'I can update name & email' do
+          editor.reload
+          expect(editor.name).to eq('different_editor name')
+          expect(editor.email).to eq('different_editor@user.com')
+        end
+        it 'I get a confirmation-message' do
+          expect(flash[:notice]).to eq('profile updated')
+        end
       end
-      it 'I get redirected to #edit-view when I make an Input failure
-      & get an error-message' do
-        sign_in editor
-        put :update, id: editor.id, user: { email: '' }
-        expect(response).to be_redirect
-        # expect(response).to redirect_to(edit_profile_path(editor))
-        expect(flash[:notice]).to eq('error')
-        put :update, id: editor.id, user: { name: '' }
-        expect(flash[:notice]).to eq('error')
+      context 'with invalid attributes' do
+        before do
+          sign_in editor
+          put :update, id: editor.id, user: { email: '' }
+        end
+        it 'I get redirected to edit-template' do
+          expect(response).to be_redirect
+        end
+        it 'I get an error-message' do
+          expect(flash[:notice]).to eq('error')
+        end
       end
-      it 'I can not update my role' do
-        sign_in editor 
-        put :update, id: editor.id, user: { role: 'alien' }
+    end
+    context 'as non-logged-in user' do
+      it 'I can not update my profile' do
+        put :update, id: editor.id, user: { name: 'different_editor name' }
         editor.reload
-        expect(editor.role).not_to eq('alien')
-        expect(editor.role).to eq('editor')
+        expect(editor.name).not_to eq('different_editor name')
+        expect(editor.name).to eq(editor.name)
       end
     end
   end
