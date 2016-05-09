baaa88d7d738ae8abdf2ec13b239e9daecddb2ab
netznarkose <mmaintz@posteo.de>
2016-05-09 10:40:48 +0200

    revised entries_controller_spec

--- spec/controllers/entries_controller_spec.rb
+++ spec/controllers/entries_controller_spec.rb
@@ -12,43 +12,21 @@ describe EntriesController, :type => :controller do
   let(:commentator) { FactoryGirl.create(:commentator) }
   let(:user) { FactoryGirl.create(:user) }
 
-  before :each do
+  before do
     admin
     editor
   end
 
-  # describe "GET index" do
-  #   it "returns published entries" do
-  #     pending("tdd?, test does not run")
-  #     published_entry
-  #     get :index
-  #     expect(assigns(:entries)).to include(published_entry)
-  #   end
-  #   it "does not returns unpublished entries" do
-  #     pending("tdd?, test does not run")
-  #     unpublished_entry 
-  #     get :index
-  #     expect(assigns(:entries)).not_to include(published_entry)
-  #   end
-  # end
-
   describe 'get index' do
-    context 'as admin' do
-      it 'shows user index' do
-        pending('todo')
-        raise
-        sign_in admin
-        get :index
-        expect(response).to be_success
-      end
-      it 'sorts the index in the right way' do
-        pending('todo')
-        raise
-      end
-      it 'shows the right search results' do
-        pending('todo')
-        raise
-      end
+    subject { get :index }
+    it_behaves_like 'every user can access'
+    it 'sorts the index in the right way' do
+      pending('todo')
+      raise
+    end
+    it 'shows the right search results' do
+      pending('todo')
+      raise
     end
   end
 
@@ -94,6 +72,16 @@ describe EntriesController, :type => :controller do
   end
 
   describe "GET new" do
+    context 'as admin, chiefeditor and editor' do
+      subject { get :new }
+
+      it_behaves_like 'something that admin, chiefeditor & editor can access'
+    end
+    context 'as commentator and guest' do
+      subject { get :new }
+
+      it_behaves_like 'something that commentator and guest can not access'
+    end
     it "assigns a new entry as @entry" do
       sign_in admin
       get :new
@@ -102,15 +90,23 @@ describe EntriesController, :type => :controller do
   end
 
   describe "GET edit" do
-    before :each do
-      unpublished_entry
+    before do
+      entry
+    end
+    context 'as admin, chiefeditor and editor' do
+      subject { get :edit, id: entry.id }
+
+      it_behaves_like 'something that admin, chiefeditor & editor can access'
+    end
+    context 'as commentator and guest' do
+      subject { get :edit, id: entry.id }
+
+      it_behaves_like 'something that commentator and guest can not access'
     end
     it "assigns the requested entry as @entry" do
-      sign_in editor
-      unpublished_entry.user_id = editor.id
-      unpublished_entry.reload
-      get :edit, id: unpublished_entry.id
-      expect(assigns(:entry)).to eq(unpublished_entry)
+      sign_in admin
+      get :edit, id: entry.id
+      expect(assigns(:entry)).to eq(entry)
     end
   end
 
@@ -121,9 +117,8 @@ describe EntriesController, :type => :controller do
       end
       context "for herself" do
         it "creates an entry" do
-          attributes = FactoryGirl.attributes_for(:entry, user_id: admin.id) 
-          expect {
-            post :create, entry: attributes
+            expect {
+              post :create, entry: FactoryGirl.attributes_for(:entry, user_id: admin.id) 
           }.to change(Entry, :count).by(1)
           assigns(:entry).tap do |entry|
             expect(entry.user).to eq(admin)
@@ -136,7 +131,6 @@ describe EntriesController, :type => :controller do
       end
       context "for somebody else" do
         it "creates an entry" do
-          editor 
           expect {
             post :create, entry: FactoryGirl.attributes_for(:entry, user_id: editor.id)
           }.to change(Entry, :count).by(1)
@@ -156,9 +150,8 @@ describe EntriesController, :type => :controller do
       end
       context "for herself" do
         it "creates an entry" do
-          attributes = FactoryGirl.attributes_for(:entry, user_id: chiefeditor.id) 
           expect {
-            post :create, entry: attributes
+            post :create, entry: attributes = FactoryGirl.attributes_for(:entry, user_id: chiefeditor.id) 
           }.to change(Entry, :count).by(1)
           assigns(:entry).tap do |entry|
             expect(entry.user).to eq(chiefeditor)
@@ -171,7 +164,6 @@ describe EntriesController, :type => :controller do
       end
       context "for somebody else" do
         it "creates an entry" do
-          editor 
           expect {
             post :create, entry: FactoryGirl.attributes_for(:entry, user_id: editor.id)
           }.to change(Entry, :count).by(1)
@@ -191,9 +183,8 @@ describe EntriesController, :type => :controller do
       end
       context "for herself" do
         it "creates an entry" do
-          attributes = FactoryGirl.attributes_for(:entry, user_id: editor.id) 
           expect {
-            post :create, entry: attributes
+            post :create, entry: FactoryGirl.attributes_for(:entry, user_id: editor.id) 
           }.to change(Entry, :count).by(1)
           assigns(:entry).tap do |entry|
             expect(entry.user).to eq(editor)
@@ -206,17 +197,16 @@ describe EntriesController, :type => :controller do
       end
       context "for somebody else" do
         it "does not creates an entry" do
-          editor 
           expect {
             post :create, entry: FactoryGirl.attributes_for(:entry, user_id: chiefeditor.id)
           }.to change(Entry, :count).by(0)
           end
         end
-        it "and gets redirects to the new template" do
+        it "gets redirects to the new-template" do
           post :create, entry: FactoryGirl.attributes_for(:entry, user_id: chiefeditor.id)
-          expect(response).to be_redirect
+          expect(response).to redirect_to(new_entry_path)
         end
-        it "and gets this error-message" do
+        it "and gets an error-message" do
           post :create, entry: FactoryGirl.attributes_for(:entry, user_id: chiefeditor.id)
           expect(flash[:notice]).to eq('as editor you are not allowed to create an entry for somebody else' )
         end
@@ -232,29 +222,29 @@ describe EntriesController, :type => :controller do
       before do
         sign_in admin 
       end
-      context 'for himself' do
+      context 'own entry' do
         before do
           modified_entry_user_id = admin.id
           put :update, id: entry.id, entry: { japanische_umschrift: 'some editing on my own entry', user_id: modified_entry_user_id }
           entry.reload
         end
-        it 'updates an entry' do
+        it 'gets updated' do
           expect(entry.japanische_umschrift).to eq('some editing on my own entry')
         end
-        it 'and redirect to it' do
+        it 'and gets redirect to it' do
           expect(response).to redirect_to(entry)
         end
       end
-      context 'for somebody else' do
+      context 'other users entry' do
         before do
           modified_entry_user_id = editor.id
           put :update, id: entry.id, entry: { japanische_umschrift: 'some editing on somebody else\'s entry', user_id: modified_entry_user_id }
           entry.reload
         end
-        it 'updates an entry' do
+        it 'gets updated' do
           expect(entry.japanische_umschrift).to eq('some editing on somebody else\'s entry')
         end
-        it 'and redirects to it' do
+        it 'and gets redirect to it' do
           expect(response).to redirect_to(entry)
         end
       end
@@ -263,29 +253,29 @@ describe EntriesController, :type => :controller do
       before do
         sign_in chiefeditor 
       end
-      context 'for himself' do
+      context 'own entry' do
         before do
           modified_entry_user_id = chiefeditor.id
           put :update, id: entry.id, entry: { japanische_umschrift: 'some editing on my own entry', user_id: modified_entry_user_id }
           entry.reload
         end
-        it 'updates an entry' do
+        it 'gets updated' do
           expect(entry.japanische_umschrift).to eq('some editing on my own entry')
         end
-        it 'and redirect to it' do
+        it 'and gets redirect to it' do
           expect(response).to redirect_to(entry)
         end
       end
-      context 'for somebody else' do
+      context 'other users entry' do
         before do
           modified_entry_user_id = admin.id
           put :update, id: entry.id, entry: { japanische_umschrift: 'some editing on somebody else\'s entry', user_id: modified_entry_user_id }
           entry.reload
         end
-        it 'updates an entry' do
+        it 'gets updated' do
           expect(entry.japanische_umschrift).to eq('some editing on somebody else\'s entry')
         end
-        it 'and redirects to it' do
+        it 'and gets redirect to it' do
           expect(response).to redirect_to(entry)
         end
       end
@@ -295,32 +285,32 @@ describe EntriesController, :type => :controller do
       before do
         sign_in editor
       end
-      context 'for himself' do
+      context 'own entry' do
         before do
           modified_entry_user_id = editor.id
           put :update, id: entry.id, entry: { japanische_umschrift: 'some editing on my own entry', user_id: modified_entry_user_id }
           entry.reload
         end
-        it 'updates an entry' do
+        it 'gets updated' do
           expect(entry.japanische_umschrift).to eq('some editing on my own entry')
         end
-        it 'and redirect to it' do
+        it 'and gets redirect to it' do
           expect(response).to redirect_to(entry)
         end
       end
-      context 'for somebody else' do
+      context 'other users entry' do
         before do
           modified_entry_user_id = chiefeditor.id
           put :update, id: entry.id, entry: { japanische_umschrift: 'some editing on somebody else\'s entry', user_id: modified_entry_user_id }
           entry.reload
         end
-        it 'does not updates an entry' do
+        it 'does not get updated' do
           expect(entry.japanische_umschrift).not_to eq('some editing on somebody else\'s entry')
         end
-        it 'and gets redirected' do
+        it 'gets redirected' do
           expect(response).to be_redirect
         end
-        it 'and gets this error-message' do
+        it 'and gets an error-message' do
           expect(flash[:notice]).to eq('as editor you are not allowed to edit somebody else\'s entry' )
         end
       end
--- spec/support/authorization_examples.rb
+++ spec/support/authorization_examples.rb
@@ -1,3 +1,89 @@
+shared_examples_for 'every user can access' do
+  context 'as admin' do
+    let(:admin) { FactoryGirl.create(:admin) }
+    before do
+      sign_in admin 
+    end
+    it 'is accessible' do
+      subject
+      expect(response).to be_success 
+    end
+  end
+  context 'as chiefeditor' do
+    let(:chiefeditor) { FactoryGirl.create(:chiefeditor) }
+    before do
+      sign_in chiefeditor 
+    end
+    it 'is accessible' do
+      subject
+      expect(response).to be_success 
+    end
+  end
+  context 'as editor' do
+    let(:editor) { FactoryGirl.create(:editor) }
+    before do
+      sign_in editor 
+    end
+    it 'is accessible' do
+      subject
+      expect(response).to be_success 
+    end
+  end
+  context 'as commentator' do
+    let(:editor) { FactoryGirl.create(:editor) }
+    before do
+      sign_in commentator 
+    end
+    it 'is accessible' do
+      subject
+      expect(response).to be_success 
+    end
+  end
+  context 'as guest' do
+    let(:editor) { FactoryGirl.create(:editor) }
+    before do
+      sign_in guest 
+    end
+    it 'is accessible' do
+      subject
+      expect(response).to be_success 
+    end
+  end
+end
+
+shared_examples_for 'something that admin, chiefeditor & editor can access' do
+  context 'as admin' do
+    let(:admin) { FactoryGirl.create(:admin) }
+    before do
+      sign_in admin 
+    end
+    it 'is accessible' do
+      subject
+      expect(response).to be_success 
+    end
+  end
+  context 'as chiefeditor' do
+    let(:chiefeditor) { FactoryGirl.create(:chiefeditor) }
+    before do
+      sign_in chiefeditor 
+    end
+    it 'is accessible' do
+      subject
+      expect(response).to be_success 
+    end
+  end
+  context 'as editor' do
+    let(:editor) { FactoryGirl.create(:editor) }
+    before do
+      sign_in editor 
+    end
+    it 'is accessible' do
+      subject
+      expect(response).to be_success 
+    end
+  end
+end
+
 shared_examples_for 'something that only admin can access' do
   context 'when user is a chiefeditor' do
     let(:chiefeditor) { FactoryGirl.create(:chiefeditor) }
@@ -41,7 +127,7 @@ shared_examples_for 'something that only admin can access' do
   end
 end
 
-shared_examples_for 'something that only admin, chiefeditor & editor can access' do
+shared_examples_for 'something that commentator and guest can not access' do
   context 'when user is a commentator' do
     let(:commentator) { FactoryGirl.create(:commentator) }
     before do
