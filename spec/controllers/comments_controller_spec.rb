54e87f5806349668f24c30a42e245a10c6263818
netznarkose <mmaintz@posteo.de>
2016-05-16 12:36:11 +0200

    revised comment_controller_spec and shared_examples/authorization_examples

--- spec/controllers/comments_controller_spec.rb
+++ spec/controllers/comments_controller_spec.rb
@@ -10,10 +10,8 @@ describe CommentsController, type: :controller do
 
 
   describe 'GET edit' do
-    it 'edits a comment' do
-      get :edit, entry_id: comment.entry_id, id: comment.id
-      expect(response).to render_template('edit')
-    end
+    subject { get :edit, entry_id: comment.entry_id, id: comment.id }
+    it_behaves_like 'something that admin, chiefeditor, editor and commentator can access'
   end
 
   describe 'POST create' do
@@ -45,38 +43,319 @@ describe CommentsController, type: :controller do
         end
       end
     end
+    context 'as chiefeditor' do
+      before do
+        sign_in chiefeditor 
+      end
+      context 'with valid attributes' do
+        it 'creates a comment' do
+          attributes = FactoryGirl.attributes_for(:comment)
+          expect {
+            post :create, entry_id: attributes[:entry_id], comment: attributes
+          }.to change(Comment, :count).by(1)
+          assigns(:comment).tap do |comment|
+            expect(comment).to be_valid
+          end
+        end
+      end
+      context 'with invalid attributes' do
+        it 'does not creates a comment' do
+          attributes = FactoryGirl.attributes_for(:comment, comment: '')
+          expect {
+            post :create, entry_id: attributes[:entry_id], comment: attributes
+          }.to change(Comment, :count).by(0)
+          assigns(:comment).tap do |comment|
+            expect(comment).not_to be_valid
+          end
+          expect(response).to render_template('entries/show')
+        end
+      end
+    end
+    context 'as editor' do
+      before do
+        sign_in editor 
+      end
+      context 'with valid attributes' do
+        it 'creates a comment' do
+          attributes = FactoryGirl.attributes_for(:comment)
+          expect {
+            post :create, entry_id: attributes[:entry_id], comment: attributes
+          }.to change(Comment, :count).by(1)
+          assigns(:comment).tap do |comment|
+            expect(comment).to be_valid
+          end
+        end
+      end
+      context 'with invalid attributes' do
+        it 'does not creates a comment' do
+          attributes = FactoryGirl.attributes_for(:comment, comment: '')
+          expect {
+            post :create, entry_id: attributes[:entry_id], comment: attributes
+          }.to change(Comment, :count).by(0)
+          assigns(:comment).tap do |comment|
+            expect(comment).not_to be_valid
+          end
+          expect(response).to render_template('entries/show')
+        end
+      end
+    end
+    context 'as commentator' do
+      before do
+        sign_in commentator 
+      end
+      context 'with valid attributes' do
+        it 'creates a comment' do
+          attributes = FactoryGirl.attributes_for(:comment)
+          expect {
+            post :create, entry_id: attributes[:entry_id], comment: attributes
+          }.to change(Comment, :count).by(1)
+          assigns(:comment).tap do |comment|
+            expect(comment).to be_valid
+          end
+        end
+      end
+      context 'with invalid attributes' do
+        it 'does not creates a comment' do
+          attributes = FactoryGirl.attributes_for(:comment, comment: '')
+          expect {
+            post :create, entry_id: attributes[:entry_id], comment: attributes
+          }.to change(Comment, :count).by(0)
+          assigns(:comment).tap do |comment|
+            expect(comment).not_to be_valid
+          end
+          expect(response).to render_template('entries/show')
+        end
+      end
+    end
     context 'as non-logged-in user' do
       it 'does not creates a comment' do
-        attributes = FactoryGirl.attributes_for(:comment, comment: '')
+        attributes = FactoryGirl.attributes_for(:comment)
         expect {
           post :create, entry_id: attributes[:entry_id], comment: attributes
         }.to change(Comment, :count).by(0)
-      end
-      it 'and gets redirected' do
         expect(response).to redirect_to(root_path)
         expect(flash[:notice]).to eq('Access denied!')
       end
     end
   end
   describe 'GET update' do
-    it 'updates a comment' do
-      put :update, entry_id: comment.entry_id, id: comment.id,
-        comment: { comment: 'hey some changes in the content' }
-      comment.reload
-      expect(comment.comment).to eq('hey some changes in the content')
+    context 'as admin' do
+      before do
+        sign_in admin 
+      end
+      context 'own comments' do
+        before do
+          comment
+          comment.update(user_id: admin.id)
+        end
+        it 'can be updated' do
+          put :update, entry_id: comment.entry_id, id: comment.id,
+            comment: { comment: 'hey some changes in the content' }
+          comment.reload
+          expect(comment.comment).to eq('hey some changes in the content')
+        end
+      end
+    end
+    context 'as chiefeditor' do
+      before do
+        sign_in chiefeditor 
+      end
+      context 'own comments' do
+        before do
+          comment
+          comment.update(user_id: chiefeditor.id)
+        end
+        it 'can be updated' do
+          put :update, entry_id: comment.entry_id, id: comment.id,
+            comment: { comment: 'hey some changes in the content' }
+          comment.reload
+          expect(comment.comment).to eq('hey some changes in the content')
+        end
+      end
+    end
+    context 'as editor' do
+      before do
+        sign_in editor 
+      end
+      context 'own comments' do
+        before do
+          comment
+          comment.update(user_id: editor.id)
+        end
+        it 'can be updated' do
+          put :update, entry_id: comment.entry_id, id: comment.id,
+            comment: { comment: 'hey some changes in the content' }
+          comment.reload
+          expect(comment.comment).to eq('hey some changes in the content')
+        end
+      end
+    end
+    context 'as commentator' do
+      before do
+        sign_in commentator 
+      end
+      context 'own comments' do
+        before do
+          comment
+          comment.update(user_id: commentator.id)
+        end
+        it 'can be updated' do
+          put :update, entry_id: comment.entry_id, id: comment.id,
+            comment: { comment: 'hey some changes in the content' }
+          comment.reload
+          expect(comment.comment).to eq('hey some changes in the content')
+        end
+      end
+    end
+    context 'as guest' do
+    before do
+    end
+      it 'does not updates a comment' do
+        put :update, entry_id: comment.entry_id, id: comment.id,
+          comment: { comment: 'hey some changes in the content' }
+        comment.reload
+        expect(comment.comment).not_to eq('hey some changes in the content')
+        expect(comment.comment).to eq(comment.comment)
+      end
     end
   end
 
   describe 'DELETE destroy' do
-    before do
-      comment
+    context 'as admin' do
+      before do
+        sign_in admin
+      end
+      context 'own comments' do
+        before do
+          comment
+          comment.update(user_id: admin.id)
+        end
+        it 'can be deleted' do
+          expect{
+            delete :destroy, entry_id: comment.entry_id, id: comment.id
+          }.to change(Comment, :count).by(-1)
+          expect(response).to redirect_to(entry_path(comment.entry))
+          expect(flash[:notice]).to eq('Kommentar erfolgreich gelöscht.')
+        end
+      end
+      context 'other users comments' do
+        before do
+          comment
+          comment.update(user_id: editor.id)
+        end
+        it 'can be deleted' do
+          expect{
+            delete :destroy, entry_id: comment.entry_id, id: comment.id
+          }.to change(Comment, :count).by(-1)
+          expect(response).to redirect_to(entry_path(comment.entry))
+          expect(flash[:notice]).to eq('Kommentar erfolgreich gelöscht.')
+        end
+      end
     end
-    it 'deletes a comment' do
-      expect{
-        delete :destroy, entry_id: comment.entry_id, id: comment.id
-      }.to change(Comment, :count).by(-1)
-      expect(response).to redirect_to(entry_path(comment.entry))
-      expect(flash[:notice]).not_to be_empty
+    context 'as chiefeditor' do
+      before do
+        sign_in chiefeditor 
+      end
+      context 'own comments' do
+        before do
+          comment
+          comment.update(user_id: chiefeditor.id)
+        end
+        it 'can be deleted' do
+          expect{
+            delete :destroy, entry_id: comment.entry_id, id: comment.id
+          }.to change(Comment, :count).by(-1)
+          expect(response).to redirect_to(entry_path(comment.entry))
+          expect(flash[:notice]).to eq('Kommentar erfolgreich gelöscht.')
+        end
+      end
+      context 'other users comments' do
+        before do
+          comment
+          comment.update(user_id: editor.id)
+        end
+        it 'can be deleted' do
+          expect{
+            delete :destroy, entry_id: comment.entry_id, id: comment.id
+          }.to change(Comment, :count).by(-1)
+          expect(response).to redirect_to(entry_path(comment.entry))
+          expect(flash[:notice]).to eq('Kommentar erfolgreich gelöscht.')
+        end
+      end
+    end
+    context 'as editor' do
+      before do
+        sign_in editor 
+      end
+      context 'own comments' do
+        before do
+          comment
+          comment.update(user_id: editor.id)
+        end
+        it 'can be deleted' do
+          expect{
+            delete :destroy, entry_id: comment.entry_id, id: comment.id
+          }.to change(Comment, :count).by(-1)
+          expect(response).to redirect_to(entry_path(comment.entry))
+          expect(flash[:notice]).to eq('Kommentar erfolgreich gelöscht.')
+        end
+      end
+      context 'other users comments' do
+        before do
+          comment
+          comment.update(user_id: admin.id)
+        end
+        it 'can not be deleted' do
+          expect{
+            delete :destroy, entry_id: comment.entry_id, id: comment.id
+          }.to change(Comment, :count).by(0)
+          expect(response).to redirect_to(entry_path(comment.entry))
+          expect(flash[:notice]).to eq('editors are not allowed to update somebody else\s comment')
+        end
+      end
+    end
+    context 'as commentator' do
+      before do
+        sign_in commentator 
+      end
+      context 'own comments' do
+        before do
+          comment
+          comment.update(user_id: commentator.id)
+        end
+        it 'can be deleted' do
+          expect{
+            delete :destroy, entry_id: comment.entry_id, id: comment.id
+          }.to change(Comment, :count).by(-1)
+          expect(response).to redirect_to(entry_path(comment.entry))
+          expect(flash[:notice]).to eq('Kommentar erfolgreich gelöscht.')
+        end
+      end
+      context 'other users comments' do
+        before do
+          comment
+          comment.update(user_id: admin.id)
+        end
+        it 'can not be deleted' do
+          expect{
+            delete :destroy, entry_id: comment.entry_id, id: comment.id
+          }.to change(Comment, :count).by(0)
+          expect(response).to redirect_to(entry_path(comment.entry))
+          expect(flash[:notice]).to eq('commentators are not allowed to update somebody else\s comment')
+        end
+      end
+    end
+    context 'as guest' do
+      before do
+        comment
+      end
+      it 'comments can not be deleted' do
+        expect{
+          delete :destroy, entry_id: comment.entry_id, id: comment.id
+        }.to change(Comment, :count).by(0)
+        expect(response).to redirect_to(root_path)
+        expect(flash[:notice]).to eq('Access denied!')
+      end
     end
   end
 end
--- spec/controllers/shared_examples/authorization_examples.rb
+++ spec/controllers/shared_examples/authorization_examples.rb
@@ -47,6 +47,56 @@ shared_examples_for 'every user can access' do
   end
 end
 
+shared_examples_for 'something that admin, chiefeditor, editor and commentator can access' do
+  context 'as admin' do
+    let(:admin) { FactoryGirl.create(:admin) }
+    before do
+      sign_in admin 
+    end
+    it 'is accessible' do
+      subject
+      expect(response).to be_success 
+    end
+  end
+  context 'as chiefeditor' do
+    let(:chiefeditor) { FactoryGirl.create(:chiefeditor) }
+    before do
+      sign_in chiefeditor 
+    end
+    it 'is accessible' do
+      subject
+      expect(response).to be_success 
+    end
+  end
+  context 'as editor' do
+    let(:editor) { FactoryGirl.create(:editor) }
+    before do
+      sign_in editor 
+    end
+    it 'is accessible' do
+      subject
+      expect(response).to be_success 
+    end
+  end
+  context 'as commentator' do
+    let(:commentator) { FactoryGirl.create(:commentator) }
+    before do
+      sign_in commentator 
+    end
+    it 'is accessible' do
+      subject
+      expect(response).to be_success 
+    end
+  end
+  context 'when user is a guest' do
+    it 'redirects' do
+      subject
+      expect(response).to redirect_to(root_path)
+      expect(flash[:notice]).to eq('Access denied!')
+    end
+  end
+end
+
 shared_examples_for 'something that admin, chiefeditor & editor can access' do
   context 'as admin' do
     let(:admin) { FactoryGirl.create(:admin) }
