1f35b61757491d555429c3f811cce759a572133e
netznarkose <mmaintz@posteo.de>
2016-05-17 21:48:35 +0200

    added a current_user method to application_controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,73 +11,6 @@ class ApplicationController < ActionController::Base
     flash[:notice] = "Access denied!"
     redirect_to root_path
   end
-  # if user is logged in, return current_user, else return guest_user
-  def current_or_guest_user
-    if current_user
-      if session[:guest_user_id] && session[:guest_user_id] != current_user.id
-        logging_in
-        # reload guest_user to prevent caching problems before destruction
-        guest_user(with_retry = false).reload.try(:destroy)
-        session[:guest_user_id] = nil
-      end
-      current_user
-    else
-      guest_user
-    end
-  end
-
-  # find guest_user object associated with the current session,
-  # creating one as needed
-  def guest_user(with_retry = true)
-    # Cache the value the first time it's gotten.
-    @cached_guest_user ||= User.find(session[:guest_user_id] ||= create_guest_user.id)
-
-  rescue ActiveRecord::RecordNotFound # if session[:guest_user_id] invalid
-     session[:guest_user_id] = nil
-     guest_user if with_retry
-  end
-
-
-
-
-
-  protected
-
-  # called (once) when the user logs in, insert any code your application needs
-  # to hand off from guest_user to current_user.
-  def logging_in
-    # For example:
-    # guest_comments = guest_user.comments.all
-    # guest_comments.each do |comment|
-      # comment.user_id = current_user.id
-      # comment.save!
-    # end
-  end
-
-  def create_guest_user
-    u = User.create(:name => "guest", :email => "guest_#{Time.now.to_i}#{rand(100)}@example.com", :role => 'guest')
-    u.save!(:validate => false)
-    session[:guest_user_id] = u.id
-    u
-  end
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.for(:account_update) << :email
@@ -114,6 +47,9 @@ class ApplicationController < ActionController::Base
   def current_user?
     @user = User.find(params[:id])
     current_user.id == @user.id
+  end
+  def current_user
+    super || User.new #methoden auftruf der superklasse
   end
 
 end
