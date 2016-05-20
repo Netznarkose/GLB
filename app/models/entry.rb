5795a19e6acbd171fed7f778007280b801bad0cf
netznarkose <mmaintz@posteo.de>
2016-05-20 15:48:32 +0200

    added freigeschaltet to allowed params

--- app/models/entry.rb
+++ app/models/entry.rb
@@ -7,7 +7,7 @@ class Entry < ActiveRecord::Base
                     :alternative_japanische_lesungen, :schreibvarianten, :deutsche_uebersetzung, 
                     :lemma_art, :jahreszahlen, :uebersetzung, :quellen, :literatur, :eigene_ergaenzungen, 
                     :quellen_ergaenzungen, :literatur_ergaenzungen, :page_reference, :romaji_order, 
-                    :lemma_in_katakana, :lemma_in_lateinbuchstaben, :user_id]
+                    :lemma_in_katakana, :lemma_in_lateinbuchstaben, :user_id, :freigeschaltet]
 
   belongs_to :user
   has_many :comments
