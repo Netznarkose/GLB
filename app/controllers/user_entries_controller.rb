6117420e8afadbbb0470938349836888f4cde126
netznarkose <mmaintz@posteo.de>
2016-06-15 17:37:37 +0200

    cleanup user_entries_controller

--- app/controllers/user_entries_controller.rb
+++ app/controllers/user_entries_controller.rb
@@ -1,19 +1,24 @@
 class UserEntriesController < ApplicationController
+  before_action :find_user, only: :index
   def index
-    @user = User.find(params[:user_id])
-    search_me = params[:search]
-    unless search_me == nil || search_me == ""
-      @user_entries = @user.entries.search(params[:search])
+    if search_item = params[:search]
+      @user_entries = @user.entries.search(search_item)
     else
-      @user_entries = @user.entries.order(sort_column + " " + sort_direction).page(params[:page])
+      @user_entries = @user.entries.order(sort_column + ' ' + sort_direction).page(params[:page])
     end
   end
+
   private
-  def sort_column
-    Entry.column_names.include?(params[:sort]) ? params[:sort] : "kennzahl"
+
+  def find_user
+    @user = User.find(params[:user_id])
   end
-  
+
+  def sort_column
+    Entry.column_names.include?(params[:sort]) ? params[:sort] : 'kennzahl'
+  end
+
   def sort_direction
-    %w[asc desc].include?(params[:direction]) ? params[:direction] : "desc"
+    %w[asc desc].include?(params[:direction]) ? params[:direction] : 'desc'
   end
 end
