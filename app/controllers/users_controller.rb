ace03d8a78a743abf4eaf2128e15419214844eb4
netznarkose <mmaintz@posteo.de>
2016-06-18 12:35:32 +0200

    cleanup users_controller and users_controller_spec

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,6 @@
 class UsersController < ApplicationController
   load_and_authorize_resource
-  before_action :find_user, only: [:show, :update, :destroy]
-  # before_action :protect_from_non_admins, except: :edit
-  # before_action :protect_from_non_currents, only: [:edit, :update]
 
-  # GET /users
-  # GET /users.json
   def index
     @users = User.all
     respond_to do |format|
@@ -14,8 +9,6 @@ class UsersController < ApplicationController
     end
   end
 
-  # GET /users/1
-  # GET /users/1.json
   def show
     respond_to do |format|
       format.html # show.html.erb
@@ -23,8 +16,6 @@ class UsersController < ApplicationController
     end
   end
 
-  # GET /users/new
-  # GET /users/new.json
   def new
     @user = User.new
     respond_to do |format|
@@ -33,8 +24,6 @@ class UsersController < ApplicationController
     end
   end
 
-  # POST /users
-  # POST /users.json
   def create
     @user = User.new(user_params)
     respond_to do |format|
@@ -42,35 +31,27 @@ class UsersController < ApplicationController
         format.html { redirect_to users_path, notice: 'Mitarbeiter erfolgreich erstellt!' }
         format.json { render json: @user, role: :created, location: @user }
       else
-        format.html { redirect_to new_user_path, notice: @user.errors.messages.values.flatten.uniq.join('<br/>') }
-        format.json { render json: @user.errors, role: :unprocessable_entity } 
+        format.html { redirect_to new_user_path, notice: @user.errors.messages.values.flatten.uniq.join('<br/>') } # ?
+        format.json { render json: @user.errors, role: :unprocessable_entity }
       end
     end
   end
 
-  # GET /users/1/edit
   def edit
-    if current_user?
-      redirect_to edit_profile_path(@user) 
-    end
   end
 
-  # PUT /users/1
-  # PUT /users/1.json
   def update
     respond_to do |format|
       if @user.update(user_params)
         format.html { redirect_to user_path, notice: 'User was successfully updated.' }
         format.json { head :no_content }
       else
-        format.html { redirect_to edit_user_path(@user), notice: @user.errors.messages.values.flatten.uniq.join('<br />') }
+        format.html { redirect_to edit_user_path(@user), notice: @user.errors.messages.values.flatten.uniq.join('<br />') } # ?
         format.json { render json: @user.errors, role: :unprocessable_entity }
       end
     end
   end
 
-  # DELETE /users/1
-  # DELETE /users/1.json
   def destroy
     respond_to do |format|
       @user.destroy
@@ -83,29 +64,5 @@ class UsersController < ApplicationController
 
   def user_params
     params.require(:user).permit(:name, :email, :password, :password_confirmation, :role)
-  end
-
-  def find_user
-    @user = User.find(params[:id])
-  end
-
-  def protect_from_non_admins
-    handle_unauthorized unless admin?
-  end
-
-  def protect_from_non_currents
-    handle_unauthorized unless current_user?
-  end
-
-  def handle_unauthorized
-    unless admin?
-      respond_to do |format|
-        format.html do
-          flash[:notice] = 'Access denied!'
-          redirect_to root_path
-        end
-        format.json { render nothing: true, status: :unauthorized }
-      end
-    end
   end
 end
--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -1,14 +1,14 @@
 require 'spec_helper'
 
 describe UsersController, type: :controller do
-  let(:ulrich_appel) { FactoryGirl.create(:admin, email: 'ulrich.apel@uni-tuebingen.de')}
+  let(:ulrich_appel) { FactoryGirl.create(:admin, email: 'ulrich.apel@uni-tuebingen.de') }
   let(:admin) { FactoryGirl.create(:admin) }
   let(:chiefeditor) { FactoryGirl.create(:chiefeditor) }
   let(:editor) { FactoryGirl.create(:editor) }
   let(:user) { FactoryGirl.create(:user) }
 
   describe 'get index' do
-    subject { get :index } 
+    subject { get :index }
 
     it_behaves_like 'something that admin, chiefeditor & editor can access'
     it_behaves_like 'something that commentator and guest can not access'
@@ -16,92 +16,122 @@ describe UsersController, type: :controller do
 
   describe 'get new' do
     context 'as admin' do
-      it 'assigns a new user to @User & renders #new-view' do
+      before do
         sign_in admin
         get :new
-        expect(assigns(:user)).to be_a_new(User)
+      end
+      it 'renders the view' do
         expect(response).to render_template :new
       end
+      it 'assigns a new user-instance' do
+        expect(assigns(:user)).to be_a_new(User)
+      end
     end
-    subject { get :new } 
+    context 'as non-admin' do
+      subject { get :new }
 
-    it_behaves_like 'something that only admin can access'
+      it_behaves_like 'something that only admin can access'
+    end
   end
 
   describe 'POST create' do
     context 'as admin' do
       context 'with valid attributes' do
-        it 'creates a new user, redirects to index-template
-        & shows a confirmation-message' do
+        before do
           sign_in admin
+          post :create, user: FactoryGirl.attributes_for(:user)
+        end
+        it 'creates a new user' do
           expect {
             post :create, user: FactoryGirl.attributes_for(:user)
           }.to change(User, :count).by(1)
+        end
+        it 'redirect to the new user' do
           expect(response).to redirect_to(users_path)
+        end
+        it 'shows a confirmation message' do
           expect(flash[:notice]).to eq('Mitarbeiter erfolgreich erstellt!')
         end
       end
       context 'with invalid attributes' do
-        it 'does not create a new user, redirects to the new-template
-        & shows an error-message' do
+        before do
           sign_in admin
+          post :create, user: FactoryGirl.attributes_for(:user, name: '')
+        end
+        it 'does not create a new user' do
           expect {
             post :create, user: FactoryGirl.attributes_for(:editor, name: '')
           }.to_not change(User, :count)
+        end
+        it 'redirects to the new-template' do
           expect(response).to redirect_to(new_user_path)
+        end
+        it 'shows an error-message' do
           expect(flash[:notice]).not_to be_empty
         end
       end
     end
-    subject { post :create, user: FactoryGirl.attributes_for(:user) } 
+    context 'as non-admin' do
+      subject { post :create, user: FactoryGirl.attributes_for(:user) }
 
-    it_behaves_like 'something that only admin can access'
+      it_behaves_like 'something that only admin can access'
+    end
   end
 
   describe 'get edit' do
     context 'as admin' do
-      it 'I can visit the edit template' do
-        sign_in admin 
+      it 'renders the view' do
+        sign_in admin
         get :edit, id: editor.id
         expect(response).to render_template(:edit)
       end
     end
-    subject { post :edit, id: editor.id } 
+    context 'as non-admin' do
+      subject { post :edit, id: editor.id }
 
-    it_behaves_like 'something that only admin can access'
+      it_behaves_like 'something that only admin can access'
+    end
   end
 
   describe 'get update' do
     context 'as admin' do
+      before do
+        sign_in admin
+      end
       it 'I can update someone elses role' do
-        sign_in admin 
         put :update, id: editor.id, user: { role: 'admin' }
         editor.reload
         expect(editor.role).to eq('admin')
       end
     end
-    subject { put :update, id: editor.id, user: { role: 'admin' } } 
+    context 'as non-admin' do
+      subject { put :update, id: editor.id, user: { role: 'admin' } }
 
-    it_behaves_like 'something that only admin can access'
+      it_behaves_like 'something that only admin can access'
+    end
   end
 
 
   describe 'DELETE destroy' do
     before do
       user
-      ulrich_appel 
+      ulrich_appel
     end
     context 'as admin' do
-      it 'I can delete users and get redirected to users index' do
+      before do
         sign_in admin
+      end
+      it 'I can delete users and get redirected to users index' do
         expect{
           delete :destroy, id: user.id
         }.to change(User, :count).by(-1)
         expect(response).to redirect_to(users_path)
       end
     end
-    subject { delete :destroy, id: user.id } 
+    context 'as non-admin' do
+      subject { delete :destroy, id: user.id }
 
-    it_behaves_like 'something that only admin can access'
+      it_behaves_like 'something that only admin can access'
+    end
   end
 end
