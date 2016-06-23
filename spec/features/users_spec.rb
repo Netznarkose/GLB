d56ea0fd9f568be02f80df8d9dd882e8b720c44a
netznarkose <mmaintz@posteo.de>
2016-06-23 13:02:37 +0200

    cleanup and fixing of features/users_spec

--- spec/features/users_spec.rb
+++ spec/features/users_spec.rb
@@ -1,45 +1,40 @@
 require 'spec_helper'
 
-describe "users management" do
+describe 'users management' do # check test-descriptions and redundancies between controller-tests
   describe 'authentication' do
     it 'successfull login' do
-      user = FactoryGirl.create(:user) 
-      visit root_path 
+      user = FactoryGirl.create(:user)
+      visit root_path
       click_link('Login')
-      fill_in "user_email", with: user.email 
-      fill_in "user_password", with: user.password 
+      fill_in 'user_email', with: user.email
+      fill_in 'user_password', with: user.password
       click_button('Anmelden')
-      expect(page).to have_content("Erfolgreich angemeldet.") 
+      expect(page).to have_content('Erfolgreich angemeldet.')
     end
     it 'unsuccessfull login' do
-      visit root_path 
+      visit root_path
       click_link('Login')
-      fill_in "user_email", with: 'user@example.com' 
-      fill_in "user_password", with: 'password' 
+      fill_in 'user_email', with: 'user@example.com'
+      fill_in 'user_password', with: 'password'
       click_button('Anmelden')
       expect(page).to have_content("E-Mail-Adresse oder Passwort ist ungültig.")
     end
     it 'logout' do
-      user = FactoryGirl.create(:user) 
-      visit root_path 
+      user = FactoryGirl.create(:user)
+      visit root_path
       click_link('Login')
-      fill_in "user_email", with: user.email 
-      fill_in "user_password", with: user.password 
+      fill_in 'user_email', with: user.email
+      fill_in 'user_password', with: user.password
       click_button('Anmelden')
       click_link(user.name)
-      click_link "Abmelden" 
-      expect(page).to have_content("Erfolgreich abgemeldet.")
+      click_link 'Abmelden'
+      expect(page).to have_content('Erfolgreich abgemeldet.')
     end
   end
   describe 'authorization' do
-    it "should not show users list to common user" do
-      pending('there is no user sign up functionality')
-      click_link "abmelden"
-      fill_in "Email", :with => @editor.email
-      fill_in "Password", :with => "anything"
-      click_button "Sign in"
+    it 'should not show users list to not logged in users' do
       visit users_path
-      page.should have_content("Access denied!")
+      expect(page).to have_content('Access denied!')
     end
   end
 
@@ -48,50 +43,45 @@ describe "users management" do
     let(:admin_in_index) { User.first }
     before(:each) do
       visit new_user_session_path
-      fill_in "user_email", with: admin.email
-      fill_in "user_password", with: admin.password
+      fill_in 'user_email', with: admin.email
+      fill_in 'user_password', with: admin.password
       click_button('Anmelden')
-      @users = User.all
     end
-    it "should display users list" do
+    it 'should display users list' do
       visit users_path
-      page.should have_content("Alle Mitarbeiter")
+      expect(page).to have_content('Alle Mitarbeiter')
     end
     it "should show user's information" do
       visit users_path
       first(:link, 'Anzeigen').click
-      page.should have_content("Einträge")
+      expect(page).to have_content('Einträge')
     end
     it "can change user's role" do
-      pending('can not click the link status ändern')
       visit users_path
-      last(:link, 'Status Ändern').click
-      page.should have_content("Status Ändern")
-
-      # select "editor", :from => "user_role"
-      # find("#update_user").click
-      # admin_in_index.reload
-      # expect(admin_in_index.role).to eq('editor')
-      # admin_in_index.role.should == "editor"
+      click_link "Status Ändern"
+      select 'Editor', from: 'user_role'
+      click_button('Speichern')
+      admin_in_index.reload
+      expect(admin_in_index.role).to eq('Editor')
     end
   end
 
-  describe "user can change his profile" do
+  describe 'user can change his profile' do
     let(:admin) { FactoryGirl.create(:admin) }
     before(:each) do
       visit new_user_session_path
-      fill_in "user_email", with: admin.email
-      fill_in "user_password", with: admin.password
+      fill_in 'user_email', with: admin.email
+      fill_in 'user_password', with: admin.password
       click_button('Anmelden')
     end
-    it "should show edit page" do
-      click_link "#{admin.name}"
-      click_link "Profil Bearbeiten"
-      page.should have_content("Profil bearbeiten")
-      fill_in "Name", :with => "Tim Tom Tam"
-      click_button("Speichern")
+    it 'should show edit page' do
+      click_link admin.name.to_s
+      click_link 'Profil Bearbeiten'
+      expect(page).to have_content('Profil bearbeiten')
+      fill_in 'Name', with: 'Tim Tom Tam'
+      click_button('Speichern')
       admin.reload
-      admin.name.should == "Tim Tom Tam"
+      expect(admin.name).to eq('Tim Tom Tam')
     end
   end
 end
